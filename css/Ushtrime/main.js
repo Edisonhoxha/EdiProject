bb75d4f53455bb0760bd8b31b5abc59ddb47622a
Edisonhoxha <eh49285@universum-ks.org>
2021-01-14 13:33:34 +0100

    new files added

---
+++ css/Ushtrime/main.js
@@ -0,0 +1,29 @@
+// window.alert('script loaded');
+// var testVariable = '12345678'
+
+// if (email && password) {
+// 	 return true;
+// }
+// else{
+// 	return false;
+
+// }
+
+// if(2+2 === 4){
+// 	console.log('rezultati eshte 4')
+// }
+
+// function login(email,password){
+// 	console.l
+// }
+
+function getData(){
+	
+	console.log('data returned')
+
+}
+function openModal(){
+	this.getData();
+	console.log('modal opened')
+	
+}
